492c7d8f1294adf3de4662ca18a66ffe06b4e978
公羊八木 <debug0man@126.com>
2022-01-12 10:39:40 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -12,4 +12,5 @@
 
 # Uncomment a feed source
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+git clone https://github.com/destan19/OpenAppFilter.git package/OpenAppFilter
 # Add a feed source
